eed438cbc487d4bb2fca30efa46b150cd8fa0413
CODESBYMENG <codesbymeng@gmail.com>
2019-09-04 10:11:06 +0200

    [NEW] homepage template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/homepage'
 	root 'home#index'
+	root 'welcome#homepage'
 	devise_for :users
 	resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_105022) do
+ActiveRecord::Schema.define(version: 2019_09_03_081936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.string "description"
+    t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
