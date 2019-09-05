71e6481012396bfce1f1656c08ca80bdcd18a250
jayrom3 <jerome@lena-fr.com>
2019-09-05 11:26:49 +0200

    [NEW] Add product controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
 	root 'home#index'
 	devise_for :users
 	resources :users
 
 	resources :orders
-  resources :product
+  resources :products
   resources :carts do
     resources :line_items
   end
